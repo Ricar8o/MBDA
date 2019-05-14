8992ff38ac365de71d2aab098e77cdad28405f9f
Ricar8o <45279329+Ricar8o@users.noreply.github.com>
2019-05-14 14:38:29 -0500

    Update CRUDI.sql

--- GutierrezMartinez/proyecto/CRUDI.sql
+++ GutierrezMartinez/proyecto/CRUDI.sql
@@ -1,14 +1,23 @@
 CREATE OR REPLACE PACKAGE BODY PC_PRESTAMO IS 
-  PROCEDURE AD_PRESTAMO (xempleado IN VARCHAR, xlibro IN VARCHAR, xafiliado IN VARCHAR) IS
+    PROCEDURE AD_PRESTAMO (xempleado IN VARCHAR, xlibro IN VARCHAR, xafiliado IN VARCHAR) IS
     BEGIN
-        INSERT INTO PRESTAMOS(empleado,libro,afiliado) values(xempleado, xlibro, xafiliado);
+        INSERT INTO PRESTAMOS(empleadoReg,libro,afiliado) values(xempleado, xlibro, xafiliado);
         COMMIT;
     EXCEPTION
         WHEN OTHERS THEN 
             ROLLBACK;
             RAISE_APPLICATION_ERROR(-20001, 'No se pudo realizar el prestamo');
     END;
+    PROCEDURE DEVOLVER_lIBRO (xempleado IN VARCHAR, xlibro IN VARCHAR) IS
+    BEGIN
+        UPDATE PRESTAMOS SET empleadoEnt= xempleado WHERE empleadoEnt IS NULL AND LIBRO=xlibro;
+        COMMIT;
+    EXCEPTION
+        WHEN OTHERS THEN 
+            ROLLBACK;
+            RAISE_APPLICATION_ERROR(-20001, 'No se pudo realizar la devolucion');
+    END;
 END PC_PRESTAMO;
 
-
-EXECUTE pc_prestamo.ad_PRESTAMO('000018','pgk292','000001');
+EXECUTE pc_prestamo.ad_PRESTAMO('000018','000001','000001');
+execute PC_PRESTAMO.DEVOLVER_LIBRO('000018','000001');
